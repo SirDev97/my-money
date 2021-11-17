e856da3e8d3f2729829381ed904dc7a02c536307
SirDev97 <alkin971@gmail.com>
2021-11-17 15:19:03 +0100

    add cleanup functions in hooks

--- src/hooks/useLogout.js
+++ src/hooks/useLogout.js
@@ -1,8 +1,9 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { projectAuth } from '../firebase/config';
 import { useAuthContext } from './useAuthContext';
 
 export const useLogout = () => {
+  const [isCancelled, setIsCancelled] = useState(false);
   const [error, setError] = useState(null);
   const [isPending, setIsPending] = useState(false);
   const { dispatch } = useAuthContext();
@@ -16,13 +17,27 @@ export const useLogout = () => {
 
       dispatch({ type: 'LOGOUT' });
 
+      if (!isCancelled) {
+        setIsPending(false);
+        setError(null);
+      }
+
       setIsPending(false);
       setError(null);
     } catch (err) {
-      console.log(err.message);
-      setError(err);
-      setIsPending(false);
+      if (!isCancelled) {
+        console.log(err.message);
+        setError(err);
+        setIsPending(false);
+      }
     }
   };
+
+  useEffect(() => {
+    return () => {
+      setIsCancelled(true);
+    };
+  }, []);
+
   return { logout, error, isPending };
 };
