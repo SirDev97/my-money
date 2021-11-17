b69a1c9d543681c213aeeefe6867877d0b5f81df
SirDev97 <alkin971@gmail.com>
2021-11-17 13:35:54 +0100

    using signup hook

--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -1,4 +1,6 @@
+// hooks
 import { useState } from 'react';
+import { useSignup } from '../../hooks/useSignup';
 
 // styles
 import styles from './Signup.module.css';
@@ -7,11 +9,11 @@ export default function Signup() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [displayName, setDisplayName] = useState('');
+  const { signup, isPending, error } = useSignup();
 
   const handleSubmit = (e) => {
     e.preventDefault();
-
-    console.log(email, password, displayName);
+    signup(email, password, displayName);
   };
 
   return (
@@ -45,7 +47,9 @@ export default function Signup() {
         />
       </label>
 
-      <button className="btn">Signup</button>
+      {!isPending && <button className="btn">Signup</button>}
+      {isPending && <button className="btn">loading</button>}
+      {error && <p>{error}</p>}
     </form>
   );
 }
