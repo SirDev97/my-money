3fcca1429e83b66c5f655332d7ba4786f1586c41
SirDev97 <alkin971@gmail.com>
2021-11-18 13:07:06 +0100

    add watching for firebase auth state changes and waiting until it is ready

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
+import { useAuthContext } from './hooks/useAuthContext';
+
 // components
 import Navbar from './components/Navbar';
 
@@ -9,22 +11,26 @@ import Login from './pages/login/Login';
 import Signup from './pages/signup/Signup';
 
 function App() {
+  const { authIsReady } = useAuthContext();
+
   return (
     <div className="App">
-      <BrowserRouter>
-        <Navbar />
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/login">
-            <Login />
-          </Route>
-          <Route path="/signup">
-            <Signup />
-          </Route>
-        </Switch>
-      </BrowserRouter>
+      {authIsReady && (
+        <BrowserRouter>
+          <Navbar />
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/login">
+              <Login />
+            </Route>
+            <Route path="/signup">
+              <Signup />
+            </Route>
+          </Switch>
+        </BrowserRouter>
+      )}
     </div>
   );
 }
