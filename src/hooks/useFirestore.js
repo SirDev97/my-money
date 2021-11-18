47a64d536d8d7c938f9a97fc40c7855c58d0e452
SirDev97 <alkin971@gmail.com>
2021-11-18 13:41:44 +0100

    create useFirestore hook template

---
+++ src/hooks/useFirestore.js
@@ -0,0 +1,33 @@
+import { useReducer, useEffect, useState } from 'react';
+import { projectFirestore } from '../firebase/config';
+
+let initialState = {
+  document: null,
+  isPending: false,
+  error: null,
+  success: null,
+};
+
+const firestoreReducer = (state, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export const useFirestore = (collection) => {
+  const [response, dispatch] = useReducer(firestoreReducer, initialState);
+  const [isCancelled, setIsCancelled] = useState(false);
+
+  const ref = projectFirestore.collection(collection);
+
+  const addDocument = async (doc) => {};
+
+  const deleteDocument = async (id) => {};
+
+  useEffect(() => {
+    return () => setIsCancelled(true);
+  }, []);
+
+  return { addDocument, deleteDocument, response };
+};
